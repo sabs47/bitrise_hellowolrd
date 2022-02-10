19e24c63aefa1a5364f8975627bb17d84e0e81b0
lv <hadjisabslv@gmail.com>
2022-02-10 14:44:35 +0100

    hello world bitrise

---
+++ hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by medoune on 10/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by medoune on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
